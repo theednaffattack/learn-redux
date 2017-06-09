1984f8eee122a0db61ed674267a7a3759303d33e
Eddie Naff <eddienaff@gmail.com>
2017-06-09 12:17:33 -0700

    Add photo grid to index route

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Single extends React.Component{
+  render() {
+    return (
+      <div className=".photo-grid">
+        I'm the photo grid
+      </div>
+    );
+  }
+};
+
+export default Single;
