fcee3ddf9f8418d8894e8a4fcb31e3be3a293b2c
Eddie Naff <eddienaff@gmail.com>
2017-06-12 17:29:47 -0700

    Add redux promise to handle async requests

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,6 +1,7 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
+import ReduxPromise from 'redux-promise';
 
 // import the root reducer
 import rootReducer from './reducers/index';
@@ -9,13 +10,15 @@ import rootReducer from './reducers/index';
 import posts from './data/posts.js';
 import comments from './data/comments.js';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 // create an object for the default data
 const defaultState = {
   posts,
   comments,
 }
 
-const store = createStore(rootReducer, defaultState);
+const store = createStoreWithMiddleware(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
